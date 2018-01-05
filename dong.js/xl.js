59d3efe8f256171d0046fb6d1867d413e90ef66f
liweijuan <2273445425@qq.com>
2018-01-05 20:27:53 +0800

    大作业

---
+++ dong.js/xl.js
@@ -0,0 +1,17 @@
+
+	    window.onload=function(){
+	    	var divContent1=document.getElementById("divContent1");
+	    	var collapseDiv=document.getElementById("collapseDiv");
+
+	    	collapseDiv.onclick=function(){
+	    		if(divContent1.style.display=="none")
+	    		{
+	    			divContent1.style.display="block";
+	    		}
+	    		else
+	    		{
+	    			divContent1.style.display="none";
+	    		}
+	    	}
+	    }
+  
